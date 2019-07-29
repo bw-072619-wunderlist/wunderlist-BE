c2d7e97f5cf2ac135946b8586fed9d520bf2f1f7
erozonachi <erozonachi.james@gmail.com>
2019-07-29 09:50:35 +0100

    Add create static method to todos model class

--- src/resources/todos/TodosModel.js
+++ src/resources/todos/TodosModel.js
@@ -1,3 +1,10 @@
 import db from '../../data/dbConfig';
 
-export class TodosModel {}
+export class TodosModel {
+  static create = (todo) => {
+    return db('todos')
+      .insert(todo)
+      .returning('*')
+      .first();
+  }
+}
