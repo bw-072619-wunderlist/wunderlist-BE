1d5a1b8035d2209688f94ed69499c0d43167d5ec
erozonachi <erozonachi.james@gmail.com>
2019-07-29 22:39:22 +0100

    Add validate-todo static method to validator class

--- src/helpers/Validator.js
+++ src/helpers/Validator.js
@@ -1,3 +1,5 @@
+import { isArray } from "util";
+
 export class Validator {
   static validateId = (req, res, next) => {
     const { id } = req.params;
@@ -59,5 +61,63 @@ export class Validator {
     return {
       pass: true
     };
+  }
+
+  static validateTodo = (req, res, next) => {
+    const { title, description, completed, scheduled_at, repeat, tasks } = req.body;
+    if(!title || title.trim() === '') {
+      return res.status(400)
+        .json({
+          message: 'Missing required title field'
+        });
+    }
+    if(description) {
+      if(description.trim() === '') {
+        return res.status(400)
+          .json({
+            message: 'description field cannot be empty'
+          });
+      }
+    }
+    if(completed) {
+      if(completed !== true && completed !== false) {
+        return res.status(400)
+          .json({
+            message: 'completed field can only hold boolean value'
+          });
+      }
+    }
+    if(scheduled_at) {
+      if(!RegExp(/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?Z?$/).test(scheduled_at)) {
+        return res.status(400)
+          .json({
+            message: 'Invalid value for scheduled_at field'
+          });
+      }
+    }
+    if(repeat) {
+      if(!['no-repeat', 'daily', 'weekly', 'monthly'].includes(repeat.toLowerCase())) {
+        return res.status(400)
+          .json({
+            message: 'Invalid value for repeat field'
+          });
+      }
+    }
+    if(!Array.isArray(tasks) || tasks.length < 1) {
+      return res.status(400)
+        .json({
+          message: 'tasks field cannot be empty list'
+        });
+    }
+    for(let i = 0; i < tasks.length; i++) {
+      const check = Validator.validateTaskObject(tasks[i]);
+      if(!check.pass) {
+        return res.status(400)
+          .json({
+            message: check.message
+          });
+      }
+    }
+    next();
   }
 }
