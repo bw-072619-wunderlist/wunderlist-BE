699883cf2cba1d0ff2a828cbc125c9052a184f02
erozonachi <erozonachi.james@gmail.com>
2019-07-29 09:37:50 +0100

    Add create static method to users model class

--- src/resources/users/UsersModel.js
+++ src/resources/users/UsersModel.js
@@ -1,5 +1,9 @@
 import db from '../../data/dbConfig';
 
 export class UsersModel {
-
+  static create = (user) => {
+    return db('users')
+      .insert(user)
+      .returning('id', 'email');
+  }
 }
