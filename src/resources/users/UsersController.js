10a9cb962ad0a0b18154a9abbc5f29e13142d7c3
erozonachi <erozonachi.james@gmail.com>
2019-07-29 10:56:15 +0100

    Add login static method to users controller class

--- src/resources/users/UsersController.js
+++ src/resources/users/UsersController.js
@@ -24,4 +24,35 @@ export class UsersController {
       next(error);
     }
   }
+
+  static login = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await Users.read(email);
+      if(user && user.email) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(isMatch) {
+          const token = jwt.sign(user, 'WUE6nnw#j83-UWNJWGfsuj#*h', { expiresIn: '1d' });
+          return res.status(201)
+            .json({
+              ...user,
+              token
+            });
+        } else {
+          return res.status(400)
+            .json({
+              message: 'Invalid login credentials'
+            });
+        }
+      } else {
+        return res.status(400)
+          .json({
+            message: 'Invalid login credentials'
+          });
+      }
+    } catch(error) {
+      next(error);
+    }
+  }
 }
