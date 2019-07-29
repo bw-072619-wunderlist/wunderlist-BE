ebe9b0810aa0bdb01f2d5357127a4736a21778b5
erozonachi <erozonachi.james@gmail.com>
2019-07-29 11:53:00 +0100

    Add read static method to tasks controller class

--- src/resources/tasks/TasksController.js
+++ src/resources/tasks/TasksController.js
@@ -1,4 +1,5 @@
 import { TasksModel as Tasks } from './TasksModel';
+import { isArray } from 'util';
 
 export class TasksController {
   static create = async (req, res, next) => {
@@ -12,6 +13,24 @@ export class TasksController {
       const tasks = await Tasks.create(rawTasks);
       res.status(201)
         .json(tasks);
+    } catch(error) {
+      next(error);
+    }
+  }
+
+  static read = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const tasks = id? await Tasks.read(id) : await Tasks.read();
+      if(isArray(tasks) && tasks.length > 0) {
+        res.status(200)
+          .json(tasks);
+      } else {
+        res.status(404)
+          .json({
+            message: 'Todo with the supplied todo_id does not exist'
+          });
+      }
     } catch(error) {
       next(error);
     }
