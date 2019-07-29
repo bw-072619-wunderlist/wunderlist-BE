0cbc85b10bc9a132916d0d65b3ffa16e953583a8
erozonachi <erozonachi.james@gmail.com>
2019-07-29 09:43:49 +0100

    Add read static method to users model class

--- src/resources/users/UsersModel.js
+++ src/resources/users/UsersModel.js
@@ -6,4 +6,14 @@ export class UsersModel {
       .insert(user)
       .returning('id', 'email');
   }
+
+  static read = (email = null) => {
+    if(email) {
+      return db('users')
+        .where({ email })
+        .first();
+    }
+    return db('users')
+      .select('id', 'email');
+  }
 }
