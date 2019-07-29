688f558d43c0066c2b09dce79bce8c5f71f04705
erozonachi <erozonachi.james@gmail.com>
2019-07-29 16:22:41 +0100

    Add validate-id static mtheod to validator class

--- src/helpers/Validator.js
+++ src/helpers/Validator.js
@@ -1,3 +1,12 @@
 export class Validator {
-
+  static validateId = (req, res, next) => {
+    const { id } = req.params;
+    if(!Number.isInteger(id) || Number.parseInt(id, 10) <= 0) {
+      return res.status(400)
+        .json({
+          message: 'Invalid id in URL'
+        });
+    }
+    next();
+  }
 }
