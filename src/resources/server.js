5d21a5edcbe47bc2ae146b1c58e69992fd85f4cb
erozonachi <erozonachi.james@gmail.com>
2019-07-30 12:25:35 +0100

    Use error-handler class in server

--- src/resources/server.js
+++ src/resources/server.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import UsersRoute from './users/UsersRoute';
 import TodosRoute from './todos/TodosRoute';
 import TasksRoute from './tasks/TasksRoute';
+import ErrorHandler from '../helpers/ErrorHandler';
 
 const server = express();
 
@@ -14,5 +15,6 @@ server.use(express.json());
 server.use('/api/v2/auths', UsersRoute);
 server.use('/api/v2/todos', TodosRoute);
 server.use('/api/v2/tasks', TasksRoute);
+server.use(ErrorHandler.handleError);
 
 export default server;
