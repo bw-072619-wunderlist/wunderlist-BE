377a4c6e8a3dc62b0fb2e65013cfd2d07edb65c5
erozonachi <erozonachi.james@gmail.com>
2019-07-30 11:19:43 +0100

    Move tasks controller's create method
    Move tasks controller's create method to todos controller as add-todo-task
    static method

--- src/resources/tasks/TasksController.js
+++ src/resources/tasks/TasksController.js
@@ -2,22 +2,6 @@ import { TasksModel as Tasks } from './TasksModel';
 import { isArray } from 'util';
 
 export class TasksController {
-  static create = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      let rawTasks = req.body;
-      rawTasks = rawTasks.map(task => ({
-        ...task,
-        todo_id: id
-      }));
-      const tasks = await Tasks.create(rawTasks);
-      res.status(201)
-        .json(tasks);
-    } catch(error) {
-      next(error);
-    }
-  }
-
   static read = async (req, res, next) => {
     try {
       const { id } = req.params;
--- src/resources/todos/TodosController.js
+++ src/resources/todos/TodosController.js
@@ -94,4 +94,20 @@ export class TodosController {
       next(error);
     }
   }
+
+  static addTodoTask = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      let rawTasks = req.body;
+      rawTasks = rawTasks.map(task => ({
+        ...task,
+        todo_id: id
+      }));
+      const tasks = await Tasks.create(rawTasks);
+      res.status(201)
+        .json(tasks);
+    } catch(error) {
+      next(error);
+    }
+  }
 }
