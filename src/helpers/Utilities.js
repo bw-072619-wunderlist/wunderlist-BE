7b1e9fa496771d4d93fd8f20b5f8350ae67cb8ad
erozonachi <erozonachi.james@gmail.com>
2019-07-31 18:29:36 +0100

    Fix bug in start-jobs method

--- src/helpers/Utilities.js
+++ src/helpers/Utilities.js
@@ -2,17 +2,17 @@ import cron from 'node-cron';
 import { TodosModel as Todos } from '../resources/todos/TodosModel';
 
 export default class Utilities {
-  static async startJobs() {
-    try {
-      cron.schedule('0 */6 * * *', () => {
+  static startJobs() {
+    cron.schedule('0 */6 * * *', async () => {
+      try {
         await Todos.repeatTodos();
         await Todos.repeatTodos('weekly');
         await Todos.repeatTodos('monthly');
 
         await Todos.delete();
-      });
-    } catch(error) {
-      //Silence
-    }
+      } catch(error) {
+        //silence
+      }
+    });
   }
 }
