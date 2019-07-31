6cfa3a55cdeb87131483ca2308ca9dd0f0ed3277
erozonachi <erozonachi.james@gmail.com>
2019-07-31 15:26:29 +0100

    Add repeat-todos static method to todo-model class

--- src/resources/todos/TodosModel.js
+++ src/resources/todos/TodosModel.js
@@ -1,4 +1,5 @@
 import db from '../../data/dbConfig';
+import moment from 'moment';
 
 export class TodosModel {
   static create(todo) {
@@ -41,5 +42,28 @@ export class TodosModel {
       .delete()
       .where({ id })
       .returning('*');
+  }
+
+  static repeatTodos(repeat = 'daily') {
+    switch(repeat) {
+      case 'daily':
+        const nextSchedule = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString();
+        return db('todos')
+          .update({scheduled_at: nextSchedule})
+          .whereRaw("repeat != 'no-repeat' AND now() < scheduled_at::date + '1 day'::interval");
+      case 'weekly':
+        const nextSchedule = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString();
+        return db('todos')
+          .update({scheduled_at: nextSchedule})
+          .whereRaw("repeat != 'no-repeat' AND now() < scheduled_at::date + '7 day'::interval");
+      case 'monthly':
+        const nextSchedule = new Date(new Date().setDate(new Date().getDate() + 30)).toISOString();
+        return db('todos')
+          .update({scheduled_at: nextSchedule})
+          .whereRaw("repeat != 'no-repeat' AND now() < scheduled_at::date + '30 day'::interval");
+      default:
+        return null;
+    }
+    
   }
 }
