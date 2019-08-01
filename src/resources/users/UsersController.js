710a0b8f7b3598a71a8d652447c6830b00beca6b
erozonachi <erozonachi.james@gmail.com>
2019-08-01 13:03:53 +0100

    Add reset-auth-info() to the users controller

--- src/resources/users/UsersController.js
+++ src/resources/users/UsersController.js
@@ -61,6 +61,40 @@ export class UsersController {
     }
   }
 
+  static async resetAuthInfo(req, res, next) {
+    try {
+      const { email, newEmail, password, newPassword } = req.body;
+
+      const user = await Users.read(email);
+      if(user && user.email) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(isMatch) {
+          let changes = {
+            password: newPassword? bcrypt.hashSync(newPassword, 12) : user.password,
+            email: newEmail || email
+          }
+          changes = await Users.update(user.id, changes);
+
+          return res.status(200)
+            .json({
+              id: changes[0].id,
+              username: changes[0].username,
+              email: changes[0].email,
+              avatar: changes[0].avatar,
+              notify: changes[0].notify
+            });
+        } else {
+          return res.status(400)
+            .json({
+              message: 'Invalid credentials'
+            });
+        }
+      }
+    } catch(error) {
+      next(error);
+    }
+  }
+
   static async read(req, res, next) {
     try {
       const users = await Users.read();
