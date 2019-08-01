3cbf067c36e1094be4418a459e84443126114a07
erozonachi <erozonachi.james@gmail.com>
2019-08-01 11:27:33 +0100

    Add a mailer transporter to the utilities

--- src/helpers/Utilities.js
+++ src/helpers/Utilities.js
@@ -1,5 +1,14 @@
 import cron from 'node-cron';
 import { TodosModel as Todos } from '../resources/todos/TodosModel';
+import mailer from 'nodemailer';
+
+const transporter = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "wunderlistv2@gmail.com",
+    pass: "09063912145"
+  }
+});
 
 export default class Utilities {
   static startJobs() {
